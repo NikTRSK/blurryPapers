6bf659a1caa48d2210671450cbcca58a847ac99b
Ziyan Ma <ziyanma@usc.edu>
2017-03-26 10:41:05 -0700

    Hello World

---
+++ Backend/src/WebCrawler.php
@@ -0,0 +1,19 @@
+<?php 
+
+	class WebCrawler
+	{
+		private $mURL;
+		private $mText;
+
+		function __construct($mURL)
+		{
+			$this->mURL = $mURL;
+		}
+
+		function getContent($url)
+		{
+
+		}
+	}
+
+ ?>
