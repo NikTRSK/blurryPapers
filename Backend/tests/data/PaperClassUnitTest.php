e892e027e1c1b93498b20c387ce40845478a1f78
Uddhav Joglekar <joglekar@usc.edu>
2017-03-26 12:02:28 -0700

    wrote test cases for PaperClass

---
+++ Backend/tests/data/PaperClassUnitTest.php
@@ -0,0 +1,51 @@
+<?php
+
+require_once('PaperClass.php');
+
+class PaperClassUnitTest extends PHPUnit_Framework_TestCase
+{
+	// test mPaperName ecapsulation
+	public function testGetPaperNameReturnsValidPaperName()
+	{
+
+	}
+
+	public function testGetPaperNameReturnsInvalidPaperName()
+	{
+
+	}
+
+	// test mAuthorNames encapsulation
+	public function testGetAuthorNamesReturnsAuthorNames()
+	{
+		
+	}
+
+	public function testAddAuthorNameSingleAddReturnsSingleAuthor()
+	{
+
+	}
+
+	public function testAddAuthorNameMultipleAddReturnsMultipleAuthor()
+	{
+
+	}
+
+	public function testAddAuthorNameNothingAddedReturnsEmpty()
+	{
+
+	}
+
+	// test mPDFURLLink encapsulation
+	public function testGetPDFURLLinkReturnsValidPDFURLLink()
+	{
+
+	}
+
+	public function testGetPDFURLLinkReturnsInvalidPDFURLink()
+	{
+
+	}
+}
+
+?>
