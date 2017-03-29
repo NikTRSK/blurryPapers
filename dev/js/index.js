b1a739af8c0e767ee11b6e915ebbdfe57186a0fd
John Michael Bachmann <jbachman@usc.edu>
2017-03-28 17:21:03 -0700

    F: Initialize feature branch for article list page; John Michael Bachmann, Jean Tu

---
+++ dev/js/index.js
@@ -0,0 +1,10 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import { default as styles } from '../../dev/styles/style.sass';
+
+const app = document.getElementById('root');
+
+ReactDOM.render(
+	<div>
+	</div>
+	, app);
