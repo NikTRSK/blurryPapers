17e57b99065b13e3074692cfbf53c3862acd6ea2
Ziyan Ma <ziyanma@usc.edu>
2017-03-30 18:33:59 -0700

    F: WebCrawler.php; James, Uddhav

--- Backend/src/WebCrawler.php
+++ Backend/src/WebCrawler.php
@@ -40,6 +40,7 @@
 			if ($this->mConfig == "ACM")
 			{
 				$url = "http://dl.acm.org/results.cfm?query=Halfond";
+
 				$this->mURL = $url;
 
 				$ch = curl_init();
@@ -49,6 +50,8 @@
 				curl_setopt($ch, CURLOPT_CONNECTTIMEOUT, $timeout);
 				$html = curl_exec($ch);
 				curl_close($ch);
+
+				echo $html;
 
 				# Create a DOM parser object
 				$this->mDom = new DOMDocument();
@@ -81,11 +84,38 @@
 			if ($this->mConfig == "ACM")
 			{
 				$finder = new DomXPath($this->mDom);
-				$classname="title";
+				$classname="details";
 				$nodes = $finder->query("//*[contains(@class, '$classname')]");
+				// $nodes = $finder->query("//*[@class='details']");
+
 				foreach ($nodes as $key) {
-					$text .= $this->mDom->saveHTML($key);
-					echo $text . "\n";
+
+					//CRAWL TITLE
+					$titleQuery = $finder->query("./*[contains(@class, 'title')]", $key)->item(0);
+					$titleQuery1 = $finder->query("./a", $titleQuery)->item(0);
+					// $titleText = $finder->query("//a", $title)->item(0);
+					$titleText = $titleQuery1->textContent;
+					// $tempDom = 
+
+					echo $titleText."\n";
+
+
+					// ABSTRACT
+					$abstractQuery = $finder->query("./*[contains(@class, 'abstract')]", $key)->item(0);
+					$abstractText = $abstractQuery->textContent;
+					echo $abstractText. "\n";
+
+					//URL
+					$urlQuery = $finder->query("./*[contains(@class, 'ft')]", $key)->item(0);
+					$urlQuery1 = $finder->query("./a", $urlQuery)->item(0)->getAttribute('href');
+					$href = "dl.acm.org/" . $urlQuery1;
+					echo $href . "\n";
+
+					//Conference
+
+					
+
+					echo "..............................\n";
 				}
 			}
 			
